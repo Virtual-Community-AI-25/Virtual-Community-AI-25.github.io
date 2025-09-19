eeb088f2f6b5d9669559bd17e5159749f0cd32ab
Virtual-Community-AI-25 <virtual-community-ai@proton.me>
2025-09-19 13:26:40 -0400

    fix url to codebase and delete

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://virtual-community-ai.github.io/',
+  site: 'https://virtual-community-ai-25.github.io/',
   base: '/',
   integrations: [mdx()],
   vite: {
